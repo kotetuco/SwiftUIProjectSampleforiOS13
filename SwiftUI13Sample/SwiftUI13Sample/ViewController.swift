e426f53ce73d6938c71500b3251370391dbb7dea
kotetuco <paprapapra@gmail.com>
2022-01-28 09:00:14 +0900

    Add project files.

---
+++ SwiftUI13Sample/SwiftUI13Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUI13Sample
+//
+//  Created by kotetu on 2022/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
